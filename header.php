0f5f64076fc8d62a78eaafc07cedcbf5cb207748
shyju k <shyju@onetikk.com>
2019-01-16 16:59:03 +0530

    Homepage started

--- header.php
+++ header.php
@@ -19,7 +19,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/custom-style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/custom-style.css" >
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
@@ -28,11 +29,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+	<div id="wrapper-navbar" class="header-back">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav class="navbar navbar-expand-md menu-new">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container" >
@@ -66,7 +67,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						'theme_location'  => 'primary',
 						'container_class' => 'collapse navbar-collapse',
 						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
+						'menu_class'      => 'navbar-nav',
 						'fallback_cb'     => '',
 						'menu_id'         => 'main-menu',
 						'depth'           => 2,
@@ -78,5 +79,19 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<?php endif; ?>
 
 		</nav><!-- .site-navigation -->
-
+		<div class="container mt-5">
+			<div class="jumbotron bg-none text-white">
+			  <div class="container">
+			    <h1 class="display-4">AL MADAR HOLDING WLL</h1>
+			    <p class="lead">
+			    	Al Madar Holding W.L.L. is a Qatari based Property Development
+					Company with over 24 years’ experience and a formidable reputation
+					for quality and service.
+				</p>
+				<p class="lead">
+				   <a class="btn btn-header btn-lg" href="#" role="button">Learn more</a>
+				</p>
+			  </div>
+			</div>
+		</div>
 	</div><!-- #wrapper-navbar end -->
