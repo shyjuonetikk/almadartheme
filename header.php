e15bced3e6aa5c5e2e219d898933e0ded7f1b64f
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-01-16 17:50:53 +0530

    Commit by jithin

--- header.php
+++ header.php
@@ -7,76 +7,76 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/custom-style.css" >
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
 
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar" class="header-back">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'understrap');?></a>
 
 		<nav class="navbar navbar-expand-md menu-new">
 
-		<?php if ( 'container' == $container ) : ?>
+		<?php if ('container' == $container): ?>
 			<div class="container" >
-		<?php endif; ?>
+		<?php endif;?>
 
 					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+					<?php if (!has_custom_logo()) {?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+						<?php if (is_front_page() && is_home()): ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name');?></a></h1>
 
-						<?php else : ?>
+						<?php else: ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name');?></a>
 
-						<?php endif; ?>
+						<?php endif;?>
 
 
 					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
+	the_custom_logo();
+}?><!-- end custom logo -->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap');?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
 				<!-- The WordPress Menu goes here -->
 				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			<?php if ( 'container' == $container ) : ?>
+	array(
+		'theme_location' => 'primary',
+		'container_class' => 'collapse navbar-collapse',
+		'container_id' => 'navbarNavDropdown',
+		'menu_class' => 'navbar-nav',
+		'fallback_cb' => '',
+		'menu_id' => 'main-menu',
+		'depth' => 2,
+		'walker' => new Understrap_WP_Bootstrap_Navwalker(),
+	)
+);?>
+			<?php if ('container' == $container): ?>
 			</div><!-- .container -->
-			<?php endif; ?>
+			<?php endif;?>
 
 		</nav><!-- .site-navigation -->
 		<div class="container mt-5">
@@ -89,7 +89,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					for quality and service.
 				</p>
 				<p class="lead">
-				   <a class="btn btn-header btn-lg" href="#" role="button">Learn more</a>
+				   <a class="btn btn-header btn-lg" href="#" role="button">Learn mor</a>
 				</p>
 			  </div>
 			</div>
