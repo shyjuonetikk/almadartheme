e8ac263fb85df866139518cfbd077ff80c89df16
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-01-19 10:17:44 +0530

    about changes

--- footer.php
+++ footer.php
@@ -7,18 +7,18 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull');?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="wrapper float-left" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+	<div class="<?php echo esc_attr($container); ?>">
 
 		<div class="row">
 
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<?php understrap_site_info();?>
 
 					</div><!-- .site-info -->
 
@@ -44,7 +44,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- #page we need this extra closing tag here -->
 
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 
 </body>
 
--- template-about-page.php
+++ template-about-page.php
@@ -4,5 +4,119 @@
  */
 get_header();
 ?>
+<div class="col-xl-12 float-left">
+	<div class="container">
+		<div id="msg-chairman" class="col-xl-12 mb-5 p-0 float-left">
+			<div class="col-12 m-0 p-0 text-center text-white about-bg mb-5">
+				<h3 class="px-5 py-3 font-weight-normal">A Message from the chairman</h3>
+			</div>
+			<div class="container">
+				<div class="col-lg-5 float-left text-center">
+					<img class="img_eng" src="http://localhost/almadar/wp-content/themes/almadar/img/About/Engineer. Ali Abdul Reda Mashhadi.png" alt="">
+				</div>
+				<div class="col-lg-1 float-left text-center">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/About/Line_about.svg">
+				</div>
+				<div class="col-lg-6 float-left">
+					<p>Our main focus has always been to deliver the very best quality combined absolute professionalism and the utmost integrity throughout the Company in every area of our activity. With over 600 employees we have successfully delivered 160 buildings and landmarks since the establishment of the business 25 years ago. Al Madar Holding W.L.L is proud to continue contributing to the ever improving international skylines. The economic and constant infrastructure improvements in Qatar under the expert guidance of H.H. Sheikh Tamim Bin Hamad Al-Thani, the Emir apparent are further evidence of the confidence in the GCC region. With multi-trades and multiple companies, the Company has expanded over the years with developments in Real Estate such as Villa Compounds, Commercial Towers and Residential Towers. The journey continues and we are now heavily investing and committing resources creating Master Plans and Joint Ventures in Qatar, U.A.E., GCC and other international countries. We look forward to further enhancing our valued and trusted international reputation.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-xl-12 float-left">
+	<div class="container">
+		<div id="msg-md" class="col-xl-12 mb-5 p-0 float-left">
+			<div class="col-12 m-0 p-0 text-center text-white about-bg mb-5">
+				<h3 class="px-5 py-3 font-weight-normal">A Message from the Managing Director</h3>
+			</div>
+			<div class="container">
+				<div class="col-lg-6 float-left">
+					<p class="card-text">Our main focus has always been to deliver the very best quality combined absolute professionalism and the utmost integrity throughout the Company in every area of our activity. With over 600 employees we have successfully delivered 160 buildings and landmarks since the establishment of the business 25 years ago. Al Madar Holding W.L.L. is proud to continue contributing to the ever improving international skylines. The economic and constant infrastructure improvements in Qatar under the expert guidance of H.H. Sheikh Tamim Bin Hamad Al-Thani, the Emir apparent are further evidence of the confidence in the GCC region. With multi-trades and multiple companies, the Company has expanded over the years with developments in Real Estate such as Villa Compounds, Commercial Towers and Residential Towers. The journey continues and we are now heavily investing and committing resources creating Master Plans and Joint Ventures in Qatar, U.A.E., GCC and other international countries. We look forward to further enhancing our valued and trusted International reputation.</p>
+				</div>
+				<div class="col-lg-1 float-left text-center">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/About/Line_about.svg">
+				</div>
+				<div class="col-lg-5 float-left text-center">
+					<img class="img_eng" src="<?php echo get_template_directory_uri(); ?>/img/About/Engineer. Ali Abdul Reda Mashhadi(1).png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-xl-12 float-left">
+	<div class="container">
+		<div id="our-mission" class="col-xl-12 mb-5 p-0 float-left">
+			<div class="container">
+				<div class="col-lg-6 float-left text-center">
+					<img class="img_eng" src="<?php echo get_template_directory_uri(); ?>/img/About/Our Mission.svg" alt="Our Mission">
+				</div>
+				<div class="col-lg-6 float-left">
+					<p>The heart and soul of our enterprise has always been our people within the organization. Nothing is more important than our integrity. This begins with insisting on absolute quality at every stage of our development.</p>
+					<p>Al Madar Holding is committed to elevate design and quality to continually improve levels, and to continue to attract, the very best expertise and professionalism in all areas of the Company.</p>
+					<p>Our focus is to maintain the credibility and vigorously protect the reputation we have already gained through diligence, dedication and our total commitment to our Clients using the most advance technologies in pursuit of continued client satisfaction.</p>
+					<p>Al Madar Company continues to further invest in all areas of the Company’s activities, ensuring the Company’s image and our landmarks are a symbol of prestige</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="col-xl-12 float-left">
+	<div class="container">
+		<div id="our-mission" class="col-xl-12 mb-5 p-0 float-left">
+			<div class="container">
+				<div class="col-lg-6 float-left text-right">
+					<p>Al Madar Holding is seeking to be in a pole position in the construction and development through carrying out huge projects of unique architectural criteria.</p>
+					<p>The investment activities of the company are accompanied by the enormous development and prosperity of the economy and construction in Qatar, where our highly rated residential blocks and administrative projects are moving up side by side with the country’s advancement.</p>
+					<p>Al Madar Holding focuses on fulfilling its future ambitions by strictly maintaining a high level of standards and technicalities that preserve the advanced position of the Company in the local and regional markets.</p>
+					<p>Al Madar Holding is committed to every plan it makes considering the
+					satisfaction of the clients and constant improvement of its activities and
+					technologies.</p>
+				</div>
+				<div class="col-lg-6 float-left text-center">
+					<img class="img_eng" src="<?php echo get_template_directory_uri(); ?>/img/About/Our Vision.svg" alt="Our Vision">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-<?php get_footer(); ?>
+<div class="col-12">
+	<div class="container">
+		<div id="about_cert">
+			<!-- <div class="col-sm-4 float-left">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/Logo-ISO_9001.svg">
+			</div>
+			<div class="col-sm-4 float-left">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/QMark Certification.svg">
+			</div> -->
+			<div class="col-sm-12 float-left">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/grp.png">
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="newsletter-container p-5">
+	<div class="col-6 m-auto text-center">
+		<h3 class="text-light prmy-font">SUBSCRIBE TO OUR NEWSLETTERS</h3>
+		<p class="text-light fs-12 mt-4 prmy-font">
+			Enter your E-mail address to receive latest news and updates. Morbi euismod
+			fermentum tempus. Mauris molestie facilisis eros id dictum.
+		</p>
+		<div class="col-12">
+			<form>
+			  <div class="form-group row">
+			    <div class="col-12 m-auto">
+			    	<div class="col-9">
+			      		<input type="email" class="form-control border-0 float-left" id="email" placeholder="YOUR EMAIL ADDRESS">
+			      	</div>
+			      	<button type="submit" class="btn-custom float-left">SUBMIT</button>
+			     </div>
+			  </div>
+			</form>
+		</div>
+	</div>
+</div> -->
+<?php get_footer();?>
