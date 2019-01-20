5cb586090325ec4542a656ccb037c2887abadffe
shyju k <shyju@onetikk.com>
2019-01-20 18:00:58 +0530

    Real estate single page started

---
+++ single-realestate.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+
+Template for displaying all posts from Realestate
+
+*/
+get_header('division');
+?>
+<div class="row m-0 p-0 realestate-back mb-5">
+	<div class="col p-5">
+		<div class="real-left-overlay">
+			<div class="row">
+				<div class="col mt-5 ml-5 pt-5">
+					<h1 class="text-white pb-3 pt-5 pl-5 pr-5">Real Estate</h1>
+					<p class="text-white  pl-5 pr-5 pb-3 w-75">Proin congue purus at metus laoreet,
+						vitae tempor dui pulvinar. Mauris eu
+						ante facilisis, hendrerit enim ut, ornare
+						sem.</p>
+					<a class="second-color pl-5 pr-5 pb-3" href="#">back</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col p-5">
+		<ul class="list-inline text-white">
+			<li>Al Madar Real Estate</li>
+			<li>Al Madar Int’l / Investment (Dubai/Germany)</li>
+			<li>Al Madar Project Mgmt.</li>
+			<li>Nobles Real Estate</li>
+		</ul>
+	</div>
+</div>
+<?php get_footer(); ?>
