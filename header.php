92327967a6550d03988741117a2933e3d68a2b34
shyju k <shyju@onetikk.com>
2019-01-22 11:44:59 +0530

    home page template added

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ $container = get_theme_mod('understrap_container_type');
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" class="header-back" style="background: url('<?php header_image(); ?>') no-repeat; background-size: cover;">
+	<div id="wrapper-navbar" class="header-back" style="background: url('<?php header_image(); ?>') no-repeat; ">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'understrap');?></a>
 
