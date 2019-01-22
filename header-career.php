7647d2a6969268a850aed7028bbea8565e0bd08a
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-01-22 18:46:16 +0530

    Career page popup

--- header-career.php
+++ header-career.php
@@ -27,6 +27,88 @@ $container = get_theme_mod('understrap_container_type');
 
 <body <?php body_class();?>>
 
+<div id="career-overlays" class="col-12 p-0 float-left">
+	<div id="career-cancel" class="col-lg-6 col-md-1 col-sm-1 col-1 float-left"></div>
+	<div class="career-form float-right w-100 px-5 py-2 bg-white col-lg-6 col-md-10 col-sm-10 col-10">
+		<h2>Submit Your CV</h2>
+		<hr>
+		<p class="text-right text-danger">* indicates required field</p>
+		<form id="career-form">
+			<div class="form-group row">
+				<label for="first name" class="col-sm-4 col-form-label">First Name<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" id="career-first-name">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="last name" class="col-sm-4 col-form-label">Last Name<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" id="career-last-name">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="Email Address" class="col-sm-4 col-form-label">Email address<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<input type="email" class="form-control" id="career-email">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="Phone Number" class="col-sm-4 col-form-label">Phone Number</label>
+				<div class="col-sm-8">
+					<input type="email" class="form-control" id="career-phone">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="Location" class="col-sm-4 col-form-label">Location<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" id="career-location">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="Nationality" class="col-sm-4 col-form-label">Nationality<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<select class="form-control" id="career-nationality">
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="function" class="col-sm-4 col-form-label">Function<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<select class="form-control" id="career-function">
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="resume" class="col-sm-4 col-form-label">Submit Your CV<span class="text-danger">*</span></label>
+				<div class="col-sm-8">
+					<input type="file" name="file" id="career-resume" class="form-control-file" data-multiple-caption="{count} files selected" multiple />
+					<label for="file" class="shadow p-2">Choose a file</label>
+				</div>
+			</div>
+			<div class="form-check mb-4">
+				<div class="custom-control custom-checkbox mb-3">
+					<input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+					<label class="custom-control-label mt-1" for="customCheck">I accept the Al Madar <span class="career-links"><a href="#">Privacy Policy</a></span> and <span class="career-links"><a href="#">Terms and conditions*</a></span></label>
+			    </div>
+			</div>
+			<div class="col-sm-12 text-right">
+				<button id="cv-button" type="submit" class="btn btn-primary text-center">Submit</button>
+			</div>
+			<p>We value your privacy.</p>
+		</form>
+	</div>
+</div>
+
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
--- page-templates/template-career.php
+++ page-templates/template-career.php
@@ -13,8 +13,8 @@ get_header('career');
 				</div>
 				<div class="container">
 					<div class="col-lg-12 float-left">
-						<p class="msg-md">Our main focus has always been to deliver the very best quality combined absolute professionalism and the utmost integrity throughout the Company in every area of our activity. With over 600 employees we have successfully delivered 160 buildings and landmarks since the establishment of the business 25 years ago. Al Madar Holding W.L.L is proud to continue contributing to the ever improving international skylines. The economic and constant infrastructure improvements in Qatar under the expert guidance of H.H. Sheikh Tamim Bin Hamad Al-Thani, the Emir apparent are further evidence of the confidence in the GCC region. With multi-trades and multiple companies, the Company has expanded over the years with developments in Real Estate such as Villa Compounds, Commercial Towers and Residential Towers. The journey continues and we are now heavily investing and committing resources creating Master Plans and Joint Ventures in Qatar, U.A.E., GCC and other international countries. We look forward to further enhancing our valued and trusted international reputation.
-						</p>
+						<p class="msg-md">At Al Madar Holding, we are rapidly expanding and continually seeking dynamic and motivated individuals to join our organization at all levels. You have a Future with us and we want to hear from you today.</p>
+						<p class="msg-md">Al Madar Holding takes pride in its Human Resources and currently employs a number of top regional and international professional and business executives. If you are a professional who has the right core skills, enthusiastic, proactive, self empowered and focused, then come join our team and be part of a driving visionary force.</p>
 					</div>
 				</div>
 			</div>
@@ -22,7 +22,7 @@ get_header('career');
 	</div>
 
 	<p id="send-cv" class="col-12 text-center float-left">
-		<a href="">SEND US YOUR CV<img src="<?php echo get_template_directory_uri(); ?>/img/arrow-dblue.png"></a>
+		<a href="#" id="resume-send">SEND US YOUR CV<img src="<?php echo get_template_directory_uri(); ?>/img/arrow-dblue.png"></a>
 	</p>
 
 	<div id="career-table-container" class="col-12 float-left mb-5 bg-white p-4">
@@ -108,63 +108,5 @@ get_header('career');
 	</div>
 </div>
 
-<div id="career-overlays" class="col-12 p-0 float-left">
-	<div class="career-form float-left w-100 px-5 py-2 bg-white col-lg-6 col-md-12 offset-lg-6">
-		<h2>Submit</h2>
-		<hr>
-		<form>
-			<div class="form-group">
-				<label for="first name">First Name<span class="text-danger">*</span></label>
-				<input type="text" class="form-control" id="career-first-name">
-			</div>
-			<div class="form-group">
-				<label for="last name">Last Name<span class="text-danger">*</span></label>
-				<input type="text" class="form-control" id="career-last-name">
-			</div>
-			<div class="form-group">
-				<label for="Email Address">Email address<span class="text-danger">*</span></label>
-				<input type="email" class="form-control" id="career-email">
-			</div>
-			<div class="form-group">
-				<label for="Phone Number">Phone Number</label>
-				<input type="email" class="form-control" id="career-phone">
-			</div>
-			<div class="form-group">
-				<label for="Location">Location<span class="text-danger">*</span></label>
-				<input type="text" class="form-control" id="career-location">
-			</div>
-			<div class="form-group">
-				<label for="Nationality">Nationality<span class="text-danger">*</span></label>
-				<select class="form-control" id="career-nationality">
-					<option>1</option>
-					<option>2</option>
-					<option>3</option>
-					<option>4</option>
-					<option>5</option>
-				</select>
-			</div>
-			<div class="form-group">
-				<label for="function">Function<span class="text-danger">*</span></label>
-				<select class="form-control" id="career-function">
-					<option>1</option>
-					<option>2</option>
-					<option>3</option>
-					<option>4</option>
-					<option>5</option>
-				</select>
-			</div>
-			<div class="form-group">
-				<label for="resume">Submit Your CV<span class="text-danger">*</span></label>
-				<input type="file" class="form-control-file" id="career-resume">
-			</div>
-			<div class="form-check">
-				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-				<label class="form-check-label" for="defaultCheck1">I accept the Al Madar <span class="career-links">Privacy Policy</span> and <span class="career-links">Terms and conditions</span></label>
-			</div>
-			<button type="submit" class="btn btn-primary text-right">Submit</button>
-			<p>We value your privacy.</p>
-		</form>
-	</div>
-</div>
-
 <?php get_footer();?>
+
