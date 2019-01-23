264042f76f2b2d0346f3c62b843db3476216aae4
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-01-23 09:57:17 +0530

    Home page responsive issues

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -1,47 +1,46 @@
 <?php
 /**
  * Template Name: Home Page
-  Description: Template for home page
+Description: Template for home page
  *
  * @package almadar
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
-<?php endif; ?>
+<?php if (is_front_page() && is_home()): ?>
+	<?php get_template_part('global-templates/hero');?>
+<?php endif;?>
 
 <div class="wrapper" id="index-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="<?php echo esc_attr($container); ?>" id="content" tabindex="-1">
 
 		<div class="row float-left">
 
 				<div class="col-12">
 					<h2 class="text-center purple-color prmy-font letter-spc-2 ptb-10">DIVISIONS</h2>
-					<?php 
-						$query = new WP_Query(array(
-						    'post_type' => array('realestate', 'construction', 'entertainment'),
-						    'post_status' => 'publish'
-						));
+					<?php
+$query = new WP_Query(array(
+	'post_type' => array('realestate', 'construction', 'entertainment'),
+	'post_status' => 'publish',
+));
 
-
-						while ($query->have_posts()) {
-						    $query->the_post();
-						    $post_id = get_the_ID();
-						    $post_title = get_the_title();
-					?>
-					<div class="col-4 float-left mb-4">
+while ($query->have_posts()) {
+	$query->the_post();
+	$post_id = get_the_ID();
+	$post_title = get_the_title();
+	?>
+					<div class="col-lg-4 col-md-6 col-sm-6 col-12 float-left mb-4">
 						<div class="thumbnail">
-						  <!-- <img src="<?php echo get_template_directory_uri();?>/img/Real Estate.png" /> -->
+						  <!-- <img src="<?php echo get_template_directory_uri(); ?>/img/Real Estate.png" /> -->
 						  <?php echo the_post_thumbnail(); ?>
 						  	<div class="h_division_title">
 						  		<h6 class="p-0"><?php echo $post_title; ?></h6>
@@ -49,11 +48,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 						</div>
 					</div>
 					<?php
-						}
+}
 
-						wp_reset_query();
-					?>
-					
+wp_reset_query();
+?>
+
 				</div>
 			<!-- </main> --><!-- #main -->
 
@@ -62,28 +61,27 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<!-- Do the right sidebar check -->
 			<?php //get_template_part( 'global-templates/right-sidebar-check' ); ?>
-			
+
 		</div><!-- .row -->
 
 	</div><!-- #content -->
 	<div class="col-12 m-0 news-section float-left">
 		<div class="container">
-			<div class="col-4 float-left">
+			<div class="col-lg-4 col-md-12 col-sm-12 float-left mb-4">
 				<div class="head pl-3"> Latest News</div>
 				<div class="thumb-box">
-					<?php 
-						$query = new WP_Query(array(
-						    'post_type' => array('almadarnews'),
-						    'category_name' => 'latestnews',
-						    'post_status' => 'publish'
-						));
+					<?php
+$query = new WP_Query(array(
+	'post_type' => array('almadarnews'),
+	'category_name' => 'latestnews',
+	'post_status' => 'publish',
+));
 
-
-						while ($query->have_posts()) {
-						    $query->the_post();
-						    $post_id = get_the_ID();
-						    $post_title = get_the_title();
-					?>
+while ($query->have_posts()) {
+	$query->the_post();
+	$post_id = get_the_ID();
+	$post_title = get_the_title();
+	?>
 					<div class="thumb-list-box float-left">
 						<div class="col-sm-12 col-md-12 col-xs-12 mt-4 pb-3">
 				              <div class="thumbnail-news-section">
@@ -96,7 +94,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					                <h6 class="p-0 m-0 mb-2 purple-color"><?php echo $post_title; ?></h6>
 					                 <p class="mb-4 purple-color"><?php echo get_the_excerpt(); ?>
 									</p>
-					                 <span class="float-left purple-color fs-10 prmy-font"><?php echo get_the_date('M j, Y');?></span>
+					                 <span class="float-left purple-color fs-10 prmy-font"><?php echo get_the_date('M j, Y'); ?></span>
 					                 <a href="#" class="float-right fs-10 second-color prmy-font">Read More</a>
 					          </div>
 						</div>
@@ -105,13 +103,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 						</div>
 					</div>
 					<?php
-						}
+}
 
-						wp_reset_query();
-					?>
+wp_reset_query();
+?>
 				</div>
 			</div> <!-- list 1 -->
-			<div class="col-4 float-left">
+			<div class="col-lg-4 col-md-12 col-sm-12 float-left mb-4">
 				<div class="head pl-3"> Completed Projects</div>
 				<div class="thumb-box">
 					<div class="thumb-list-box float-left">
@@ -176,7 +174,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					</div> <!-- thumb 3 -->
 				</div>
 			</div><!-- list 2 -->
-			<div class="col-4 float-left">
+			<div class="col-lg-4 col-md-12 col-sm-12 float-left mb-4">
 				<div class="head pl-3"> On-going projects</div>
 				<div class="thumb-box">
 					<div class="thumb-list-box float-left">
@@ -243,7 +241,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			</div><!-- list 3 -->
 		</div>
 	</div><!-- newssection -->
-	<div class="newsletter-container p-5 float-left mb-5">
+	<div class="col-xl-6 col-lg-8 col-md-8 col-12 m-auto text-center">
 		<div class="col-6 m-auto text-center">
 			<h3 class="text-light prmy-font">SUBSCRIBE TO OUR NEWSLETTERS</h3>
 			<p class="text-light fs-15 mt-4 prmy-font">
@@ -268,4 +266,4 @@ $container = get_theme_mod( 'understrap_container_type' );
 	</div>
 </div><!-- #index-wrapper -->
 
-<?php get_footer(); ?>
+<?php get_footer();?>
