7ff41a503dc8d0daa3005ae161a1e4c86e57bab0
shyju k <shyju@onetikk.com>
2019-01-26 13:31:48 +0530

    News page done

--- header-posts.php
+++ header-posts.php
@@ -22,54 +22,18 @@ $container = get_theme_mod('understrap_container_type');
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/custom-style.css" >
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
 </head>
 
 <body <?php body_class();?>>
 
 <div class="site" id="page">
-<div class="contact-pop-up">
-	<div class="row m-0">
-		<div class="col-lg-6 col-md-1 col-sm-1 col-1 m-0" id="contact-left"></div>
-		<div class="col-lg-6 col-md-10 col-sm-10 col-10 m-0 contact-popup-back p-5">
-			<h1 class="prmy-font font-weight-bold mb-4">GET IN TOUCH</h1>
-			<p class="fs-12 pb-3">For inquiries and feedback, please fill out the form below.</p>
-			<form class="w-75" id="contact-form">
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <label for="inputName" class="prmy-font">NAME</label>
-			      <input type="text" class="form-control border-0 rounded-0" id="inputName">
-			    </div>
-			    <div class="form-group col-md-6">
-			      <label for="inputCompany">COMPANY</label>
-			      <input type="text" class="form-control border-0 rounded-0" id="inputCompany">
-			    </div>
-			  </div>
-			  <div class="form-row">
-			    <div class="form-group col-md-6">
-			      <label for="inputEmail" class="prmy-font">EMAIL</label>
-			      <input type="email" class="form-control" id="inputEmail">
-			    </div>
-			    <div class="form-group col-md-6">
-			      <label for="inputSubject" class="prmy-font">SUBJECT</label>
-			      <input type="text" class="form-control border-0 rounded-0" id="inputSubject">
-			    </div>
-			  </div>
-			  <div class="form-group">
-			    <label for="inputAddress" class="prmy-font">YOUR MESSAGE</label>
-			    <textarea class="form-control border-0 rounded-0" id="inputAddress"></textarea>
-			  </div>
-			  <button type="submit" class="btn contact-btn float-right">SUBMIT</button>
-			</form>
-		</div>
-	</div>
-</div>
+
 	<!-- ******************* The Navbar Area ******************* -->
-
-	<div id="wrapper-navbar">
-
+	<div id="wrapper-navbar" class="header-about" style="background: url('<?php the_field('divisions_header_image'); ?>') no-repeat; background-size: cover;">
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e('Skip to content', 'understrap');?></a>
 
-		<nav class="navbar navbar-expand-md menu-custom" id="divisionHeader">
+		<nav class="navbar navbar-expand-md menu-new" id="myHeader">
 
 		<?php if ('container' == $container): ?>
 			<div class="container" >
@@ -84,7 +48,6 @@ $container = get_theme_mod('understrap_container_type');
 							</a>
 
 						<?php else: ?>
-
 							<a href="<?php echo esc_url(home_url('/')); ?>">
 								<img class="almadar_logo" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" />
 							</a>
@@ -121,7 +84,7 @@ $container = get_theme_mod('understrap_container_type');
 				</a>
 				<a href="#" class="social-media">
 					<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/iconfinder-icon.svg" /> -->
-					<i class="fab fa-twitter fs-22 sm-icon-tw-new pa-5 w-24"></i>
+					<i class="fab fa-twitter fs-22 sm-icon-tw pa-5 w-24"></i>
 				</a>
 				<a href="#" class="social-media">
 					<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/linkedin_blue.svg" class="social-media" />  -->
@@ -131,4 +94,12 @@ $container = get_theme_mod('understrap_container_type');
 			<?php endif;?>
 
 		</nav><!-- .site-navigation -->
+		<div class="col-md-8 mx-auto">
+			<div class="about-page">
+				<div class="container">
+					<h1 class="slider-heading display-4 mb-3 px-3 prmy-font text-white"><?php the_title(); ?></h1>
+				</div>
+			</div>
+		</div>
 	</div><!-- #wrapper-navbar end -->
+
--- single-realestate.php
+++ single-realestate.php
@@ -2,31 +2,92 @@
 
 /**
 
-Template for displaying all posts from Realestate
+Template Name: Al Madar Real Estate Page
+Description: Template for Real Estate page
 
-*/
-get_header('division');
+ */
+get_header('posts');
 ?>
-<div class="row m-0 p-5 mb-5">
-	<div class="container inner-container m-auto">
+<div class="col-lg-9 mx-auto px-3 mb-5">
+	<div class="container">
 		<div class="row m-0 p-0">
-			<div class="col float-left mb-4">
-				<div class="thumbnail">
-				  <img src="<?php echo get_template_directory_uri();?>/img/almadarrealestate2.png" />
-				  	<div class="real_division_title pl-5 w-50">
-				  		<h6 class="p-0">DUBAI</h6>
-				    </div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-12 my-2">
+				<div class="real-image-container">
+					<img class="real-image" src="<?php echo get_template_directory_uri(); ?>/img/almadarrealestate2.png" />
+					<div class="real-overlay">
+						<div class="real-title prmy-font">WHAT WE DO<div class="clear-fix"></div>
+							<img class="pt-3" src="<?php echo get_template_directory_uri(); ?>/img/down-arrow-white.png" />
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="col float-left mb-4">
-				<div class="thumbnail">
-				  <img src="<?php echo get_template_directory_uri();?>/img/almadarrealestate1.png" />
-				  	<div class="real_division_title pl-5 w-50">
-				  		<h6 class="p-0">GERMANY</h6>
-				    </div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-12 my-2">
+				<div class="real-image-container prmy-font">
+					<img class="real-image" src="<?php echo get_template_directory_uri(); ?>/img/almadarrealestate1.png" />
+					<div class="real-overlay">
+						<div class="real-title">OUR PROJECTS<div class="clear-fix"></div>
+						<img class="pt-3" src="<?php echo get_template_directory_uri(); ?>/img/down-arrow-white.png" />
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
+		<div class="row mt-5 p-5">
+			<div class="col-lg-6 col-md-12 col-sm-12 col-12 purple-color fs-12 lh-paragraph">
+				<p>Established in 1992, Al Madar Real Estate has been a significant division of Al Madar Holding.</p>
+				<p>The major achievements of the Company were based on solid foundations that include excellent planning and accurate Feasibility Studies for each and every project, the effectiveness of these foundation	are corroborated by the vivid financial returns on our company’s well calculated investments in the real estate developments without exceeding the designated budgets.</p>
+				<p>The Company is assisted by a highly qualified team players and it is	supported by the most advanced technologies and equipment’s to deliver the best results all the time.</p>
+			</div>
+			<div class="col-lg-6 col-md-12 col-sm-12 col-12">
+				<img class="img-fluid float-left float-md-left float-lg-right w-75" src="<?php echo get_template_directory_uri(); ?>/img/almadarrealestate3.png" />
+			</div>
+		</div>
+		<div class="container text-center">
+			<img class="img-fluid project-head mb-5" src="<?php echo get_template_directory_uri(); ?>/img/projectsheading.png" />
+		</div>
+		<div class="container pl-5 pr-5">
+			<div class="col-12 col-lg-12 pl-4 pr-0 mx-auto" id="projects-list">
+					<div class="row m-0 projects-list">
+					<?php
+$query = new WP_Query(array(
+	'post_type' => array('projects'),
+	'post_status' => 'publish',
+	'posts_per_page' => 2,
+));
+
+while ($query->have_posts()) {
+	$query->the_post();
+	$post_id = get_the_ID();
+	$post_title = get_the_title();
+	$post_content = get_the_excerpt();
+	$post_url = get_the_permalink();
+	$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+	?>
+						<div class="col-12 col-md-6 mb-3 pl-0">
+							<div class="card border-0 rounded-0 w-100">
+								<div class="image-container">
+							  		<img class="card-img-top project-card-image image" src="<?php echo $featured_img_url; ?>" alt="<?php echo $post_title; ?>">
+							  		<div class="overlay"></div>
+							  	</div>
+							  <div class="card-body border-0 rounded-0 pl-0 ml-0">
+							    <h6 class="card-title purple-color"><?php echo $post_title; ?></h6>
+							    <p class="card-text fs-12 purple-color"><?php echo $post_content; ?></p>
+							    <span class="purple-color float-left fs-12"><?php echo get_the_date(); ?> </span>
+							    <a href="<?php echo $post_url; ?>" class="float-right fs-12">Read More</a>
+							  </div>
+							</div>
+						</div>
+						<?php	}
+wp_reset_query();
+?>
+					</div>
+					<div class="row">
+							<div class="col m-auto text-center">
+								<a id="estate_posts"><img class="text-center" src="<?php echo get_template_directory_uri(); ?>/img/more-button.png" /> </a>
+							</div>
+					</div>
+				</div>
+		</div>
 	</div>
 </div>
-<?php get_footer(); ?>
+<?php get_footer();?>
