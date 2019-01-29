1da24947c621bf8ee2a6294bff06b5ffacde3f7f
shyju k <shyju@onetikk.com>
2019-01-29 15:04:40 +0530

    animations added for thumbs

--- functions.php
+++ functions.php
@@ -346,6 +346,7 @@ function filter_country() {
    });
 
 	$("#status-list li").on("click",function(){
+		$("#projects-list").html(data).fadeIn(500);
 			var post_type = $(this).data('post_type');
 			var post_per_page = $(this).data('posts_per_page');
 			var status = $(this).data('status_list');
@@ -358,7 +359,7 @@ function filter_country() {
 				place: place
 			},
 			 function(data){
-				 $("#projects-list").html(data);
+				 $("#projects-list").html(data).fadeIn(500);
 				});
 	});
 });
@@ -660,6 +661,7 @@ function news_filter_country() {
     // filter by news type
 
     $("#news-type li").on("click",function(){
+    	$("#projects-list").html(data).fadeOut(500);
 		var post_type = $(this).data('post_type');
 		var post_per_page = $(this).data('posts_per_page');
 		var news_type = $(this).data('news_type');
@@ -672,7 +674,7 @@ function news_filter_country() {
 			place: place
 		},
 		 function(data){
-			 $("#projects-list").html(data);
+			 $("#projects-list").html(data).fadeIn(500);
 			});
    });
 });
