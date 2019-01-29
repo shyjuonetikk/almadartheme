c8041d277fda0d5e3442f79376271fa938922866
Jithin Jayaprakash <reacherjlc@gmail.com>
2019-01-29 11:18:38 +0530

    Career Page New changes in the function field

--- single-construction.php
+++ single-construction.php
@@ -5,7 +5,7 @@
 Template Name: Al Madar construction Page
 Description: Template for construction page
 
-*/
+ */
 get_header('posts');
 ?>
 <div class="col-lg-9 mx-auto px-3 mb-5">
@@ -35,18 +35,18 @@ get_header('posts');
 		<div class="row mt-5 p-5">
 			<div class="col-lg-6 col-md-12 col-sm-12 col-12 purple-color fs-12 lh-paragraph">
 				<?php
-					while ( have_posts() ) : the_post();
-						the_content();
-						$title = get_the_title();
-						$post_id = get_the_ID();
-						$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-				?>
-				
-			</div>
-			<div class="col-lg-6 col-md-12 col-sm-12 col-12">
-				<img class="img-fluid float-left float-md-left float-lg-right w-75" src="<?php echo $featured_img_url; ?>" />
-			</div>
-		<?php endwhile; ?>
+while (have_posts()): the_post();
+	the_content();
+	$title = get_the_title();
+	$post_id = get_the_ID();
+	$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+	?>
+
+				</div>
+				<div class="col-lg-6 col-md-12 col-sm-12 col-12">
+					<img class="img-fluid float-left float-md-left float-lg-right w-75" src="<?php echo $featured_img_url; ?>" />
+				</div>
+			<?php endwhile;?>
 		</div>
 		<div class="container text-center">
 			<img class="img-fluid project-head mb-5" src="<?php echo get_template_directory_uri(); ?>/img/projectsheading.png" />
@@ -55,23 +55,23 @@ get_header('posts');
 			<div class="col-12 col-lg-12 pl-4 pr-0 mx-auto" id="projects-list">
 					<div class="row m-0 projects-list">
 					<?php
-						$query = new WP_Query(array(
-							'post_type' => array('projects'),
-							'post_status' => 'publish',
-							'meta_key'		=> 'projectdivisions',
-							'meta_value'	=> $title,
-							'posts_per_page' => 2,
-						));
+$query = new WP_Query(array(
+	'post_type' => array('projects'),
+	'post_status' => 'publish',
+	'meta_key' => 'projectdivisions',
+	'meta_value' => $title,
+	'posts_per_page' => 2,
+));
 
-						while ($query->have_posts()) {
-							$query->the_post();
-							$post_id = get_the_ID();
-							$post_title = get_the_title();
-							$post_content = get_the_excerpt();
-							$post_url = get_the_permalink();
-							$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-							?>
-						<div class="col-12 col-md-6 mb-3 pl-0">
+while ($query->have_posts()) {
+	$query->the_post();
+	$post_id = get_the_ID();
+	$post_title = get_the_title();
+	$post_content = get_the_excerpt();
+	$post_url = get_the_permalink();
+	$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+	?>
+						<div class="col-12 col-md-6 mb-3 px-4">
 							<div class="card border-0 rounded-0 w-100">
 								<div class="image-container">
 							  		<img class="card-img-top project-card-image image" src="<?php echo $featured_img_url; ?>" alt="<?php echo $post_title; ?>">
@@ -85,7 +85,8 @@ get_header('posts');
 							  </div>
 							</div>
 						</div>
-						<?php	} wp_reset_query(); ?>
+						<?php	}
+wp_reset_query();?>
 					</div>
 				</div>
 		</div>
